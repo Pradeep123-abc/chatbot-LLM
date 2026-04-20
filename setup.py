d684f7a740e56f5319a194cd93c97d3259e30716
Pradeep <pradeepagrahari995@gmail.com>
2026-04-20 12:21:39 +0530

    Fix Code

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
-from setuptools import find_package,setup
+from setuptools import find_packages,setup
 
 setup(
     name="chatbot-LLM",
     version="0.1.0",
-    auther ="Pradeep Agrahari",
-    auther_email="pradeepagrahari995@gmail.com",
-    packages=find_package(),
+    author ="Pradeep Agrahari",
+    author_email="pradeepagrahari995@gmail.com",
+    packages=find_packages(),
     install_requires=[]
 )
